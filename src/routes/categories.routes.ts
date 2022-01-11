d247abf42cd2d9583bf3cbfc74e39ee80899b378
Jandir Luiz Habitzreuter <dyjandir@hotmail.com>
2022-01-11 19:50:13 -0300

    Route categories
    converting for typescript

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,18 +1,26 @@
 import { Router } from "express";
 
+import { Category } from "../model/Category";
+
 const categoriesRoutes = Router();
 
-const categories = [];
+const categories: Category[] = [];
 
-categoriesRoutes.post("/categories", (request, response) => {
+categoriesRoutes.post("/", (request, response) => {
     const { name, description } = request.body;
 
-    categories.push({
+    const category = new Category();
+
+    // Vai passar para dentro do objeto os atributos
+    Object.assign(category, {
         name,
         description,
+        created_at: new Date(),
     });
 
-    return response.status(201).send();
+    categories.push(category);
+
+    return response.status(201).json({ category });
 });
 
 export { categoriesRoutes };
